1e94b84bdb7521d70fa93e1ba9b1062603ef9098
Ryan Connolly <ryanconnolly16@gmail.com>
2025-09-04 02:01:46 +1200

    redid UI_output file for solid principles

---
+++ src/main/java/battleship/fleetplacements/PresetPlacer.java
@@ -0,0 +1,43 @@
+package battleship.fleetplacements;
+
+import battleship.domain.Board;
+import battleship.enums.Direction;
+import battleship.enums.Result;
+import battleship.interfaces.*;
+import java.util.Random;
+
+// Single Responsibility: Random ship placement only
+class PresetPlacer implements ShipPlacer {
+    
+    public PresetPlacer() {
+        //simple constructor
+    }
+    
+    //same method name and logic as your original
+    @Override
+    public void placeShips(Fleet fleet, Board board) {
+        Random rand = new Random();
+        for(int i = 0; i < Fleet.pieces.size(); i++){
+            //will loop until the ship fit into board, including not overlapping and making sure it doesnt extend over edge of board
+            boolean placementgood = false;
+            while (placementgood == false){
+                int rshipnum = i;
+                int rxpos = rand.nextInt(10);
+                int rypos = rand.nextInt(10);
+                int rdirc = rand.nextInt(4);
+                
+                Direction dir = null;
+                if (rdirc == 0) dir = Direction.UP;
+                else if (rdirc == 1) dir = Direction.DOWN;
+                else if (rdirc == 2) dir = Direction.LEFT;
+                else if (rdirc == 3) dir = Direction.RIGHT;
+
+                Result result = fleet.PlaceShip(board, rshipnum, rxpos, rypos, dir);
+                //end loop
+                if(result == Result.OK){
+                    placementgood = true;
+                }   
+            }
+        }
+    }
+}
---
+++ src/main/java/battleship/fleetplacements/UserShipPlacer.java
@@ -0,0 +1,88 @@
+
+package battleship.fleetplacements;
+
+import battleship.domain.*;
+import battleship.enums.*;
+import battleship.interfaces.*;
+import battleship.ui.*;
+import java.util.Scanner;
+
+// Single Responsibility: User interactive ship placement only
+class UserShipPlacer implements UserPlacer {
+    private Scanner input;
+    
+    public UserShipPlacer() {
+        input = new Scanner(System.in);
+    }
+    
+    //same method name and logic as your original
+    @Override
+    public void placeShipsInteractive(Fleet fleet, Board board) {
+        while(Fleet.printinglist.size() < Fleet.pieces.size()){
+            try{
+                System.out.println("\nPlease input where you would like to place the ships in format:");
+                System.out.println("Ship number, X posistion, Y posistion, Direction");
+                System.out.println("\n" + BoardRenderer.renderBoth(board, new DefaultGlyphs()));
+                System.out.println("Ship Numbers;");
+                
+                //outputing not placed ships
+                for(int i = 0; i < Fleet.pieces.size(); i++){
+                    if (Fleet.printinglist.contains(Fleet.pieces.get(i))) continue;
+                    System.out.println((i+1) + ". " + Fleet.pieces.get(i));
+                }
+                
+                //seperating userinput to save to a list for each part
+                System.out.println("\n");
+                String posistions = UI_Output.GetInput(input);
+                String[] list = posistions.split(",");
+                    
+                int type = Integer.parseInt(list[0].trim()) -1 ;
+                
+                list[1].trim();
+                list[2].trim();
+                
+                //checking if the user input format is correct i.e. didnt put a letter for the row
+                if (( String.valueOf(type).length() > 1 ) ||
+                        ( String.valueOf(list[1]).length() > 1 || !list[1].matches("[a-zA-z]+") ) ||
+                        ( !list[2].matches("[0-9]+") )
+                        ){
+                    System.out.println("Invalid input try again.");
+                    placeShipsInteractive(fleet, board);
+                }
+                
+                //saving each part of userinput to variables 
+                int ypos;
+                int xpos = list[1].charAt(0) - 'a';
+                if(list[2].equals("10")){
+                    ypos = 9;
+                }else{
+                    ypos = list[2].charAt(0) - '0' -1;
+                }
+                String pos = list[3].trim();
+                
+                Direction dir = null;
+                
+                if (pos.contains("up")) dir = Direction.UP;
+                else if (pos.contains("down")) dir = Direction.DOWN;
+                else if (pos.contains("left")) dir = Direction.LEFT;
+                else if (pos.contains("right")) dir = Direction.RIGHT;
+                
+                //error checking for ship already placed, placement out of bounds, placing ship in a square allready taken
+                if (Fleet.printinglist.contains(Fleet.pieces.get(type))){
+                    System.out.println("Ship already placed, place another.\n\n");
+                }
+                else if(fleet.PlaceShip(board, type, xpos, ypos, dir) == Result.OK){
+                    Fleet.printinglist.add(Fleet.pieces.get(type));
+                }
+                else if(fleet.PlaceShip(board, type, xpos, ypos, dir) == Result.OCCUPIED){
+                    System.out.println("Space is occupied, try again.");
+                }
+                else if(fleet.PlaceShip(board, type, xpos, ypos, dir) == Result.OUT_OF_BOUNDS){
+                    System.out.println("Ship is out of bounds, check you origin and direction.");
+                }
+            }catch(Exception e){
+                System.out.println("Invalid input try again");
+            }
+        }
+    }
+}
